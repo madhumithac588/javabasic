8f88db0e2362903c3d7ada27086b60fd076d4fef
seccr <seccr@Madhumitha>
2024-06-27 11:41:52 +0530

    commit

---
+++ sample/src/day2/demo.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class demo {
+
+	public demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        char ch='a';
+        System.out.println(ch);
+        
+        char ch1=65;
+        System.out.println(ch1);
+        
+        char var1='\u00A7';
+        System.out.println(var1);
+        
+        char var2='\u20AC';
+        System.out.println(var2);
+        
+        int a ='A';
+        System.out.println(a);
+	}
+
+}
