6fae2c2bf64394653eecf16d83a56e26cd290ca0
seccr <seccr@Madhumitha>
2024-06-27 11:03:27 +0530

    This Is my core java project

---
+++ demo/src/day1/Sample.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.print("welcome");
+		System.out.println("to");
+		System.out.print("jsp");
+		// TODO Auto-generated method stub
+
+	}
+
+}
