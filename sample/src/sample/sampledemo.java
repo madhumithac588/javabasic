f89620e9e20503fcd90e7a21e3f73270a25c4a56
seccr <seccr@Madhumitha>
2024-06-27 11:15:04 +0530

    commit

---
+++ sample/src/sample/sampledemo.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class sampledemo {
+
+	public sampledemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello tns");
+
+	}
+
+}
