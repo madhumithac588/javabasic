aa3f898505ea44a914a1e1981eee69eca0944838
seccr <seccr@Madhumitha>
2024-06-27 11:10:01 +0530

    commithis

---
+++ demo/src/day1/demo.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class demo {
+
+	public demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.print("welcome");
+		System.out.println("to");
+		System.out.print("jsp");
+		System.out.print("jsp");
+
+	}
+
+}
